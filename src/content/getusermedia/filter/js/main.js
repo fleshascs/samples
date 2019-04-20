c58bc69511b44fc17d83127b9293f7eceaf352f1
fleshascs <35168446+fleshascs@users.noreply.github.com>
2019-04-20 17:10:39 +0300

    Update main.js

--- src/content/getusermedia/filter/js/main.js
+++ src/content/getusermedia/filter/js/main.js
@@ -84,7 +84,7 @@ function paintToCanvas(){
 
 function redEffect(pixels, threshold){
   for(let i = 0; i < pixels.data.length; i+=4){
-    pixels.data[i + 0] = pixels.data[i + 0] + 0;
+    pixels.data[i + 0] = pixels.data[i + 0] + 5;
    // pixels.data[i + 1] = pixels.data[i + 1] - 50;
     //pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
   }
