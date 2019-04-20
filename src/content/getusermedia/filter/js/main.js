0fa6f6762904b8373b0a4b494916a9e9523c16f2
fleshascs <35168446+fleshascs@users.noreply.github.com>
2019-04-20 17:44:34 +0300

    Update main.js

--- src/content/getusermedia/filter/js/main.js
+++ src/content/getusermedia/filter/js/main.js
@@ -28,7 +28,7 @@ slider.addEventListener('change', updateSliderValue);
 function updateSliderValue(e){
   const value = e.currentTarget.value;
   sliderValue.innerText = value;
-  //threshold = value;
+  threshold = 0;
   //console.log('threshold', threshold);
 }
 
