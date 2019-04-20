8cb9befc66c4f0a15e497a13ed86f2b134fd72e6
fleshascs <35168446+fleshascs@users.noreply.github.com>
2019-04-20 16:17:37 +0300

    lul

--- src/content/getusermedia/filter/js/main.js
+++ src/content/getusermedia/filter/js/main.js
@@ -57,7 +57,23 @@ function paintToCanvas(){
     canvasVideo.width = width;
     canvasVideo.height = height;
 
+    
+
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0,0, width, height);
+        pixels = redEffect(pixels);
+        ctx.putImageData(pixels, 0,0);
+        
     }, 16);
+
+}
+
+function redEffect(pixels){
+  for(let i = 0; < pixels.data.length; i+=4){
+    pixels.data[i + 0] = pixels.data[i + 0] + 100;
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+  }
+  return pixels;
 }
